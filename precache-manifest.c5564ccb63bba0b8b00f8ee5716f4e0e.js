2d2d6217ad22151d72134716d537464ff6f3086b
kima111 <kima111@gmail.com>
2020-01-23 15:28:22 -0800

    Updates

---
+++ precache-manifest.c5564ccb63bba0b8b00f8ee5716f4e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cce710c442ea2fb7680371a98860328",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "a07985ebfa5370b6b7d1",
+    "url": "/clickygame/static/css/main.74ffd4da.chunk.css"
+  },
+  {
+    "revision": "0ff44d85f266cdb27339",
+    "url": "/clickygame/static/js/2.5d25155a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/clickygame/static/js/2.5d25155a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a07985ebfa5370b6b7d1",
+    "url": "/clickygame/static/js/main.f73c3027.chunk.js"
+  },
+  {
+    "revision": "0bb000ae729cc4aafd5d",
+    "url": "/clickygame/static/js/runtime-main.592c0c47.js"
+  },
+  {
+    "revision": "195cd5234af78359a24d8e388c2df1a2",
+    "url": "/clickygame/static/media/background.195cd523.jpg"
+  }
+]);
